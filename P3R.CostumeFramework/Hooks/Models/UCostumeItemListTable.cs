f0ee0dff2addd99b639da5b5d234100d1b2d6d52
MirrorTuneZM <zutomayofan50@gmail.com>
2024-03-18 23:31:31 -0500

    Move types to own files.

---
+++ P3R.CostumeFramework/Hooks/Models/UCostumeItemListTable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using Unreal.ObjectsEmitter.Interfaces.Types;
+
+namespace P3R.CostumeFramework.Hooks.Models;
+
+[StructLayout(LayoutKind.Explicit, Size = 0x40)]
+public unsafe struct UCostumeItemListTable : IEnumerable<FCostumeItemList>
+{
+    //[FieldOffset(0x0000)] public UAppDataAsset baseObj;
+    [FieldOffset(0x0030)] public TArray<FCostumeItemList> Data;
+
+    public readonly IEnumerator<FCostumeItemList> GetEnumerator() => new TArrayWrapper<FCostumeItemList>(Data).GetEnumerator();
+
+    readonly IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
