6a56e8d4b34775e0dd60f8f2ef70843faf58b85f
RyoTune <zutomayofan50@gmail.com>
2024-09-20 07:35:29 -0500

    Refactor animation code to own types.

---
+++ P3R.CostumeFramework/Hooks/Animations/AnimationReplacer.cs
@@ -0,0 +1,57 @@
+﻿using P3R.CostumeFramework.Costumes;
+using P3R.CostumeFramework.Hooks.Animations.Models;
+using P3R.CostumeFramework.Hooks.Models;
+using P3R.CostumeFramework.Hooks.Services;
+using p3rpc.classconstructor.Interfaces;
+using Unreal.ObjectsEmitter.Interfaces.Types;
+
+namespace P3R.CostumeFramework.Hooks.Animations;
+
+public unsafe class AnimationReplacer(CharAnim type, Character target, Character replacer, IObjectMethods objMethods)
+{
+    private readonly IObjectMethods objMethods = objMethods;
+    private readonly CharAnim type = type;
+    private readonly Character target = target;
+    private readonly Character replacer = replacer;
+    private readonly string targetAnimName = AssetUtils.GetAnimName(target, type) ?? string.Empty;
+    private readonly string newAnimName = AssetUtils.GetAnimName(replacer, type) ?? string.Empty;
+
+    private UAnimSequence* targetAnim;
+    private UAnimSequence* newAnim;
+
+    private bool hasReplaced;
+
+    public void Update(UnrealObject obj)
+    {
+        if (this.hasReplaced)
+        {
+            return;
+        }
+
+        if (obj.Name.Equals(this.targetAnimName, StringComparison.OrdinalIgnoreCase))
+        {
+            this.targetAnim = (UAnimSequence*)obj.Self;
+        }
+        else if (obj.Name.Equals(this.newAnimName, StringComparison.OrdinalIgnoreCase))
+        {
+            this.newAnim = (UAnimSequence*)obj.Self;
+        }
+
+        if (this.targetAnim != null && this.newAnim != null)
+        {
+            var ogBaseObj = targetAnim->baseObj.baseObj.baseObj;
+            var ogSkel = targetAnim->baseObj.baseObj.Skeleton;
+
+            var newObj = (UAnimSequence*)this.objMethods.SpawnObject("AnimSequence", null);
+            *newObj = *this.newAnim;
+
+            *this.targetAnim = *newObj;
+
+            this.targetAnim->baseObj.baseObj.baseObj = ogBaseObj;
+            this.targetAnim->baseObj.baseObj.Skeleton = ogSkel;
+
+            this.hasReplaced = true;
+            Log.Information($"DngAnim Replaced: {this.type} || Target: {this.target} || New: {this.replacer}");
+        }
+    }
+}
---
+++ P3R.CostumeFramework/Hooks/Animations/CostumeAnimsService.cs
@@ -0,0 +1,54 @@
+﻿using P3R.CostumeFramework.Hooks.Animations.Models;
+using P3R.CostumeFramework.Hooks.Models;
+using p3rpc.classconstructor.Interfaces;
+using Unreal.ObjectsEmitter.Interfaces;
+
+namespace P3R.CostumeFramework.Hooks.Animations;
+
+internal unsafe class CostumeAnimsService
+{
+    //private readonly AnimationManager animManager;
+
+    public CostumeAnimsService(IUObjects uobjs, IObjectMethods objMethods)
+    {
+        //this.animManager = new(objMethods);
+        //uobjs.ObjectCreated += animManager.Update;
+
+        uobjs.FindObject("SKEL_Human", obj =>
+        {
+            var bones = new GameBones();
+            string[] keywords =
+            [
+                "head",
+                "eye",
+                "brow",
+                "cheek",
+                "nose",
+                "mouth",
+                "jaw",
+                "tongue",
+                "lips",
+                "hair",
+                "face",
+                "mask",
+                "iris",
+                "pupil",
+                "laugh",
+                "tooth"
+            ];
+
+            var skel = (USkeleton*)obj.Self;
+            for (int i = 0; i < skel->BoneTree.Num; i++)
+            {
+                var bone = &skel->BoneTree.AllocatorInstance[i];
+                var name = bones[i];
+                bone->TranslationRetargetingMode = EBoneTranslationRetargetingMode.OrientAndScale;
+                if (keywords.Any(x => name.Contains(x, StringComparison.OrdinalIgnoreCase)))
+                {
+                    Log.Debug($"SKEL_Human ({name}): Retarget bone animation to {EBoneTranslationRetargetingMode.OrientAndScale}.");
+                    bone->TranslationRetargetingMode = EBoneTranslationRetargetingMode.OrientAndScale;
+                }
+            }
+        });
+    }
+}
