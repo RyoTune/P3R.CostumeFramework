bc7e1a41e489ea3e3f31dae8e116df26d27c0681
RyoTune <zutomayofan50@gmail.com>
2024-09-20 10:07:07 -0500

    Set shell costume data with table service. Implement costume anim packs.

--- P3R.CostumeFramework/Hooks/Animations/CostumeAnimsService.cs
+++ P3R.CostumeFramework/Hooks/Animations/CostumeAnimsService.cs
@@ -1,19 +1,27 @@
-﻿using P3R.CostumeFramework.Hooks.Animations.Models;
+﻿using P3R.CostumeFramework.Costumes.Models;
+using P3R.CostumeFramework.Costumes;
+using P3R.CostumeFramework.Hooks.Animations.Models;
 using P3R.CostumeFramework.Hooks.Models;
-using p3rpc.classconstructor.Interfaces;
+using P3R.CostumeFramework.Hooks.Services;
 using Unreal.ObjectsEmitter.Interfaces;
+using P3R.CostumeFramework.Hooks.Costumes;
 
 namespace P3R.CostumeFramework.Hooks.Animations;
 
 internal unsafe class CostumeAnimsService
 {
+    private readonly IUnreal unreal;
+    private readonly CostumeTableService costumeTable;
+
     //private readonly AnimationManager animManager;
 
-    public CostumeAnimsService(IUObjects uobjs, IObjectMethods objMethods)
+    public CostumeAnimsService(IUObjects uobjs, IUnreal unreal, CostumeTableService costumeTable)
     {
         //this.animManager = new(objMethods);
         //uobjs.ObjectCreated += animManager.Update;
 
+        this.unreal = unreal;
+        this.costumeTable = costumeTable;
         uobjs.FindObject("SKEL_Human", obj =>
         {
             var bones = new GameBones();
@@ -42,7 +50,7 @@ internal unsafe class CostumeAnimsService
             {
                 var bone = &skel->BoneTree.AllocatorInstance[i];
                 var name = bones[i];
-                bone->TranslationRetargetingMode = EBoneTranslationRetargetingMode.OrientAndScale;
+                //bone->TranslationRetargetingMode = EBoneTranslationRetargetingMode.OrientAndScale;
                 if (keywords.Any(x => name.Contains(x, StringComparison.OrdinalIgnoreCase)))
                 {
                     Log.Debug($"SKEL_Human ({name}): Retarget bone animation to {EBoneTranslationRetargetingMode.OrientAndScale}.");
@@ -50,5 +58,63 @@ internal unsafe class CostumeAnimsService
                 }
             }
         });
+    }
+
+    public void UpdateCostumeAnims(Costume costume)
+    {
+        var charRow = this.costumeTable.GetCharacterRow(costume.Character);
+        var anims = charRow->Anims;
+
+        var commonAnim = anims.GetByIndex(0);
+        var dungeonAnim = anims.GetByIndex(1);
+        var combineAnim = anims.GetByIndex(2);
+        var eventAnim = anims.GetByIndex(3);
+
+        if (Enum.TryParse<Character>(costume.Config.Anims.Common, true, out var commonChar))
+        {
+            this.SetAnim(commonAnim, CostumeAssetType.CommonAnim, commonChar);
+        }
+        else
+        {
+            this.SetAnim(commonAnim, CostumeAssetType.CommonAnim, costume.Character);
+        }
+
+        if (Enum.TryParse<Character>(costume.Config.Anims.Dungeon, true, out var dungeonChar))
+        {
+            this.SetAnim(dungeonAnim, CostumeAssetType.DungeonAnim, dungeonChar);
+        }
+        else
+        {
+            this.SetAnim(dungeonAnim, CostumeAssetType.DungeonAnim, costume.Character);
+        }
+
+        if (Enum.TryParse<Character>(costume.Config.Anims.Combine, true, out var combineChar))
+        {
+            this.SetAnim(combineAnim, CostumeAssetType.CombineAnim, dungeonChar);
+        }
+        else
+        {
+            this.SetAnim(combineAnim, CostumeAssetType.DungeonAnim, costume.Character);
+        }
+
+        if (Enum.TryParse<Character>(costume.Config.Anims.Event, true, out var eventChar))
+        {
+            this.SetAnim(eventAnim, CostumeAssetType.EventAnim, eventChar);
+        }
+        else
+        {
+            this.SetAnim(eventAnim, CostumeAssetType.EventAnim, costume.Character);
+        }
+    }
+
+    private void SetAnim(TSoftObjectPtr<UAppCharAnimDataAsset>* charAnim, CostumeAssetType animType, Character newCharAnim)
+    {
+        var currentAnim = this.unreal.GetName(charAnim->GetObjectPtr()->ObjectId.AssetPathName);
+        var newAnim = AssetUtils.GetUnrealAssetPath(newCharAnim, 0, animType)!;
+        if (currentAnim != newAnim)
+        {
+            charAnim->baseObj.baseObj.ObjectId.AssetPathName = *this.unreal.FName(newAnim);
+            charAnim->baseObj.baseObj.WeakPtr = new();
+        }
     }
 }
--- P3R.CostumeFramework/Hooks/Services/CostumeShellService.cs
+++ P3R.CostumeFramework/Hooks/Services/CostumeShellService.cs
@@ -1,5 +1,6 @@
 ﻿using P3R.CostumeFramework.Costumes;
 using P3R.CostumeFramework.Costumes.Models;
+using P3R.CostumeFramework.Hooks.Costumes;
 using P3R.CostumeFramework.Hooks.Costumes.Models;
 using P3R.CostumeFramework.Hooks.Models;
 using p3rpc.classconstructor.Interfaces;
@@ -16,13 +17,14 @@ internal unsafe class CostumeShellService
     private readonly Dictionary<Character, nint> charRows = [];
 
     private readonly IUnreal unreal;
+    private readonly CostumeTableService costumeTable;
     private readonly CostumeRegistry costumes;
-    private bool useFemc;
 
-    public CostumeShellService(IDataTables dt, IUObjects uobjs, IUnreal unreal, CostumeRegistry costumes, IObjectMethods objMethods)
+    public CostumeShellService(IDataTables dt, IUObjects uobjs, IUnreal unreal, CostumeRegistry costumes, IObjectMethods objMethods, CostumeTableService costumeTable)
     {
         this.unreal = unreal;
         this.costumes = costumes;
+        this.costumeTable = costumeTable;
 
         dt.FindDataTable<FAppCharTableRow>("DT_Costume", table =>
         {
@@ -57,19 +59,15 @@ internal unsafe class CostumeShellService
                 this.defaultCostumes[character] = new DefaultCostume(character);
                 this.charRows[character] = (nint)charRow;
             }
-
-            this.defaultCostumes[Character.FEMC] = new FemcCostume();
         });
     }
-
-    public void SetUseFemc(bool useFemc) => this.useFemc = useFemc;
 
     public int UpdateCostume(Character character, int costumeId)
     {
         if (costumeId == SHELL_COSTUME_ID)
         {
             this.prevCostumeIds[character] = costumeId;
-            this.SetCostumeAssets(defaultCostumes[character]);
+            this.costumeTable.SetCostumeData(SHELL_COSTUME_ID, defaultCostumes[character]);
             Log.Debug($"{character}: Reset shell costume data.");
         }
 
@@ -81,131 +79,10 @@ internal unsafe class CostumeShellService
         var shouldUpdateData = this.prevCostumeIds[character] != costumeId;
         if (shouldUpdateData && this.costumes.TryGetCostume(character, costumeId, out var costume))
         {
-            this.SetCostumeAssets(costume);
-            this.SetCostumeAnims(costume);
+            this.costumeTable.SetCostumeData(SHELL_COSTUME_ID, costume);
             this.prevCostumeIds[character] = costumeId;
         }
 
         return SHELL_COSTUME_ID;
-    }
-
-    private void SetCostumeAssets(Costume costume)
-    {
-        this.SetCostumeAsset(costume, CostumeAssetType.BaseMesh);
-        this.SetCostumeAsset(costume, CostumeAssetType.CostumeMesh);
-        this.SetCostumeAsset(costume, CostumeAssetType.FaceMesh);
-        this.SetCostumeAsset(costume, CostumeAssetType.HairMesh);
-        this.SetCostumeAsset(costume, CostumeAssetType.BaseAnim);
-        this.SetCostumeAsset(costume, CostumeAssetType.CostumeAnim);
-        this.SetCostumeAsset(costume, CostumeAssetType.FaceAnim);
-        this.SetCostumeAsset(costume, CostumeAssetType.HairAnim);
-    }
-
-    private void SetCostumeAsset(Costume costume, CostumeAssetType assetType)
-    {
-        var assetFile = costume.Config.GetAssetFile(assetType) ?? this.GetDefaultAsset(costume.Character, assetType);
-        if (assetFile == null)
-        {
-            Log.Error($"Costume asset path is null.\nCostume ({costume.Character}): {costume.Name} || Asset: {assetType}");
-            return;
-        }
-
-        var assetPath = AssetUtils.GetUnrealAssetPath(assetFile);
-        if (assetType == CostumeAssetType.BaseAnim
-            || assetType == CostumeAssetType.CostumeAnim
-            || assetType == CostumeAssetType.FaceAnim
-            || assetType == CostumeAssetType.HairAnim)
-        {
-            assetPath += "_C";
-        }
-
-        var assetFName = assetFile != "None" ? *this.unreal.FName(assetPath) : *this.unreal.FName("None");
-        var data = (FAppCharCostumeData*)this.shellDataPtrs[costume.Character];
-
-        switch (assetType)
-        {
-            case CostumeAssetType.BaseMesh:
-                data->Base.Mesh.baseObj.baseObj.ObjectId.AssetPathName = assetFName;
-                data->Base.Mesh.baseObj.baseObj.WeakPtr = new();
-                break;
-            case CostumeAssetType.CostumeMesh:
-                data->Costume.Mesh.baseObj.baseObj.ObjectId.AssetPathName = assetFName;
-                data->Costume.Mesh.baseObj.baseObj.WeakPtr = new();
-                break;
-            case CostumeAssetType.HairMesh:
-                data->Hair.Mesh.baseObj.baseObj.ObjectId.AssetPathName = assetFName;
-                data->Hair.Mesh.baseObj.baseObj.WeakPtr = new();
-                break;
-            case CostumeAssetType.FaceMesh:
-                data->Face.Mesh.baseObj.baseObj.ObjectId.AssetPathName = assetFName;
-                data->Face.Mesh.baseObj.baseObj.WeakPtr = new();
-                break;
-            case CostumeAssetType.BaseAnim:
-                data->Base.Anim.baseObj.baseObj.ObjectId.AssetPathName = assetFName;
-                data->Base.Anim.baseObj.baseObj.WeakPtr = new();
-                break;
-            case CostumeAssetType.CostumeAnim:
-                data->Costume.Anim.baseObj.baseObj.ObjectId.AssetPathName = assetFName;
-                data->Costume.Anim.baseObj.baseObj.WeakPtr = new();
-                break;
-            case CostumeAssetType.HairAnim:
-                data->Hair.Anim.baseObj.baseObj.ObjectId.AssetPathName = assetFName;
-                data->Hair.Anim.baseObj.baseObj.WeakPtr = new();
-                break;
-            case CostumeAssetType.FaceAnim:
-                data->Face.Anim.baseObj.baseObj.ObjectId.AssetPathName = assetFName;
-                data->Face.Anim.baseObj.baseObj.WeakPtr = new();
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void SetCostumeAnims(Costume costume)
-    {
-        var charRow = (FAppCharTableRow*)this.charRows[costume.Character];
-        var anims = charRow->Anims;
-
-        if (Enum.TryParse<Character>(costume.Config.Anims.Common, true, out var commonAnim))
-        {
-            var anim = anims.GetByIndex(0);
-            var animAsset = AssetUtils.GetUnrealAssetPath(commonAnim, 0, CostumeAssetType.CommonAnim)!;
-            anim->baseObj.baseObj.ObjectId.AssetPathName = *this.unreal.FName(animAsset);
-            anim->baseObj.baseObj.WeakPtr = new();
-        }
-
-        if (Enum.TryParse<Character>(costume.Config.Anims.Dungeon, true, out var dungeonAnim))
-        {
-            var anim = anims.GetByIndex(1);
-            var animAsset = AssetUtils.GetUnrealAssetPath(dungeonAnim, 0, CostumeAssetType.DungeonAnim)!;
-            anim->baseObj.baseObj.ObjectId.AssetPathName = *this.unreal.FName(animAsset);
-            anim->baseObj.baseObj.WeakPtr = new();
-        }
-
-        if (Enum.TryParse<Character>(costume.Config.Anims.Combine, true, out var combineAnim))
-        {
-            var anim = anims.GetByIndex(2);
-            var animAsset = AssetUtils.GetUnrealAssetPath(combineAnim, 0, CostumeAssetType.CombineAnim)!;
-            anim->baseObj.baseObj.ObjectId.AssetPathName = *this.unreal.FName(animAsset);
-            anim->baseObj.baseObj.WeakPtr = new();
-        }
-
-        if (Enum.TryParse<Character>(costume.Config.Anims.Event, true, out var eventAnim))
-        {
-            var anim = anims.GetByIndex(3);
-            var animAsset = AssetUtils.GetUnrealAssetPath(eventAnim, 0, CostumeAssetType.EventAnim)!;
-            anim->baseObj.baseObj.ObjectId.AssetPathName = *this.unreal.FName(animAsset);
-            anim->baseObj.baseObj.WeakPtr = new();
-        }
-    }
-
-    private string? GetDefaultAsset(Character character, CostumeAssetType assetType)
-    {
-        if (character == Character.Player && this.useFemc)
-        {
-            return this.defaultCostumes[Character.FEMC].Config.GetAssetFile(assetType);
-        }
-
-        return this.defaultCostumes[character].Config.GetAssetFile(assetType);
     }
 }
